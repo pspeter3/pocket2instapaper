77aa461e6697197fd88c4052fc9231c1b05d9de6
Phips Peter <pspeter333@gmail.com>
2012-08-07 09:38:48 -0700

    The script works

--- pocket2instapaper.js
+++ pocket2instapaper.js
@@ -2,13 +2,18 @@
 var request = require('request');
 var querystring = require('querystring');
 var _ = require('underscore');
+var config = require('./config');
 
-var getPocketReadingList = function(apikey, username, password) {
-  var params = querystring.stringify({apikey: apikey, username: username, password: password});
-  request.get('https://readitlaterlist.com/v2/get?' + params, function(err, response, body) {
-    var articles = JSON.parse(body);
-    _.each(articles.list, function(article, id) {
-      console.log(article.url);
+// Get the pocket reading list
+var params = querystring.stringify(config.pocket);
+request.get('https://readitlaterlist.com/v2/get?' + params, function(err, response, body) {
+  var articles = JSON.parse(body);
+  _.each(articles.list, function(article, id) {
+    var params = querystring.stringify({username: config.instapaper.username, password: config.instapaper.password, url: article.url});
+    request.get('https://www.instapaper.com/api/add?' + params, function(err, response, body) {
+      if (body === '201') {
+        console.log('Added ' + article.url);
+      }
     });
   });
-};
+});
